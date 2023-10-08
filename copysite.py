552d4b41710add31713fe9307ab66ea57ed2d80f
Shamshi <135255247+Muxitdinovich@users.noreply.github.com>
2023-10-08 17:06:26 +0500

    Add files via upload

--- copysite.py
+++ copysite.py
@@ -3,7 +3,7 @@ from pywebcopy import save_webpage, save_website
 
 save_website(
 
-	url = "your website",
+	url = "your website", #Add your site that you want to copy
 	project_folder = "C:\ ",
 	project_name = "my_site",
 	bypass_robots = True,
